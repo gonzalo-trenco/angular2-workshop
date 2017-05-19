e7b0984526cc52270ed98d438e079799b42d87aa
Gonzalo Trenco <gtrenco@gmail.com>
2017-05-19 10:54:27 +0200

    component with Particles types and classes

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,28 +4,84 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
     <h1> My app: {{titulo}}</h1>
-    <h2 [style.fontWeight]="bold"> Details of {{particle.type}} {{particle.name}}</h2>
-    <p>Masa: {{particle.mass}}</p>
-    <p>Carga: {{particle.charge}}</p>
-    <p>Spin: {{particle.spin}}</p>
-    <p>Type: {{particle.type}}</p>
-    <p>Fuerzas: {{particle.forces.join(', ')}}</p>
-    <div>
-        <label>Ajusta la masa: </label>
-        <input [(ngModel)]="particle.mass" placeholder="masa">
-    </div>`
+    <ul class="particles">
+        <li *ngFor="let particle of particles" (click)="selectParticle(particle)">
+            <span class="code">{{particle.id}}</span>
+            {{particle.name}}
+        </li>
+    </ul>
+    <div *ngIf="selectedParticle">
+        <h2>Details for {{selectedParticle.type}} {{selectedParticle.name}}.</h2>
+        <p>Masa: {{selectedParticle.mass}}</p>
+        <p>Carga: {{selectedParticle.charge}}</p>
+        <p>Spin: {{selectedParticle.spin}}</p>
+        <p>Type: {{selectedParticle.type}}</p>
+        <p>Fuerzas: {{selectedParticle.forces.join(', ')}}</p>
+        <div>
+            <label>Ajusta la masa: </label>
+            <input [(ngModel)]="selectedParticle.mass" placeholder="masa">
+        </div>
+    </div>`,
+    styles: [`
+    .selected {
+        background-color: #CFD8DC !important;
+        color: white;
+    }
+    .particles {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 15em;
+        float: left;
+    }
+    .particles li {
+        cursor: pointer;
+        position: relative;
+        left: 0;
+        background-color: #EEE;
+        margin: .5em;
+        padding: .3em 0;
+        height: 1.6em;
+        border-radius: 20px 4px 4px 20px;
+    }
+    .particles li.selected:hover {
+        background-color: #BBD8DC !important;
+        color: white;
+    }
+    .particles li:hover {
+        color: #607D8B;
+        background-color: #DDD;
+        left: .1em;
+    }
+    .particles .text {
+        position: relative;
+        top: -3px;
+    }
+    .particles .code {
+        display: inline-block;
+        text-align: center;
+        font-size: small;
+        color: white;
+        width: 1.2em;
+        padding: 0.8em 0.7em 0 0.7em;
+        background-color: #607D8B;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: .8em;
+        border-radius: 20px;
+    }
+    `]
 })
 export class AppComponent {
     titulo: string = 'Standard model';
-    particle: Particle = {
-        id: 'u',
-        name: 'up',
-        mass: '2.3MeV/c²',
-        charge: '2/3',
-        spin: '1/2',
-        type: 'Quark',
-        forces: ['Electromagnetic', 'Strong', 'Weak']
-    };
+    public particles = PARTICLES;
+    public selectedParticle: Particle;
+    public selectParticle(particle: Particle) {
+        this.selectedParticle = particle;
+    }
 }
 
 export class Particle {
@@ -40,3 +96,22 @@ export class Particle {
 
 type ParticleType = 'Quark' | 'Lepton' | 'Boson';
 type Force = 'Strong' | 'Electromagnetic' | 'Weak';
+var PARTICLES: Particle[] = [
+    { id: 'u', name: 'up', mass: '2.3MeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+    { id: 'd', name: 'down', mass: '4.8MeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+    { id: 'c', name: 'charm', mass: '1.275GeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+    { id: 's', name: 'strange', mass: '95MeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+    { id: 't', name: 'top', mass: '173.07GeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+    { id: 'b', name: 'bottom', mass: '4.18GeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+    { id: 'e', name: 'electron', mass: '0.511MeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
+    { id: 'μ', name: 'muon', mass: '105.7MeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
+    { id: 'τ', name: 'tau', mass: '1.777GeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
+    { id: 'νe', name: 'electron neutrino', mass: '<2.2eV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
+    { id: 'νμ', name: 'muon neutrino', mass: '<0.17MeV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
+    { id: 'ντ', name: 'tau neutrino', mass: '<15.5MeV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
+    { id: 'g', name: 'gluon', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Strong'] },
+    { id: 'γ', name: 'photon', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Electromagnetic'] },
+    { id: 'Z', name: 'Z boson', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Weak'] },
+    { id: 'W', name: 'W boson', mass: '0', charge: '0', spin: '±1', type: 'Boson', forces: ['Weak'] },
+    { id: 'H', name: 'Higgs boson', mass: '0', charge: '0', spin: '0', type: 'Boson', forces: [] }
+];
--- build/app.component.js
+++ build/app.component.js
@@ -11,7 +11,7 @@ System.register(['@angular/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent, Particle;
+    var AppComponent, Particle, PARTICLES;
     return {
         setters:[
             function (core_1_1) {
@@ -21,20 +21,16 @@ System.register(['@angular/core'], function(exports_1, context_1) {
             AppComponent = (function () {
                 function AppComponent() {
                     this.titulo = 'Standard model';
-                    this.particle = {
-                        id: 'u',
-                        name: 'up',
-                        mass: '2.3MeV/c²',
-                        charge: '2/3',
-                        spin: '1/2',
-                        type: 'Quark',
-                        forces: ['Electromagnetic', 'Strong', 'Weak']
-                    };
+                    this.particles = PARTICLES;
                 }
+                AppComponent.prototype.selectParticle = function (particle) {
+                    this.selectedParticle = particle;
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1> My app: {{titulo}}</h1>\n    <h2 [style.fontWeight]=\"bold\"> Details of {{particle.type}} {{particle.name}}</h2>\n    <p>Masa: {{particle.mass}}</p>\n    <p>Carga: {{particle.charge}}</p>\n    <p>Spin: {{particle.spin}}</p>\n    <p>Type: {{particle.type}}</p>\n    <p>Fuerzas: {{particle.forces.join(', ')}}</p>\n    <div>\n        <label>Ajusta la masa: </label>\n        <input [(ngModel)]=\"particle.mass\" placeholder=\"masa\">\n    </div>"
+                        template: "\n    <h1> My app: {{titulo}}</h1>\n    <ul class=\"particles\">\n        <li *ngFor=\"let particle of particles\" (click)=\"selectParticle(particle)\">\n            <span class=\"code\">{{particle.id}}</span>\n            {{particle.name}}\n        </li>\n    </ul>\n    <div *ngIf=\"selectedParticle\">\n        <h2>Details for {{selectedParticle.type}} {{selectedParticle.name}}.</h2>\n        <p>Masa: {{selectedParticle.mass}}</p>\n        <p>Carga: {{selectedParticle.charge}}</p>\n        <p>Spin: {{selectedParticle.spin}}</p>\n        <p>Type: {{selectedParticle.type}}</p>\n        <p>Fuerzas: {{selectedParticle.forces.join(', ')}}</p>\n        <div>\n            <label>Ajusta la masa: </label>\n            <input [(ngModel)]=\"selectedParticle.mass\" placeholder=\"masa\">\n        </div>\n    </div>",
+                        styles: ["\n    .selected {\n        background-color: #CFD8DC !important;\n        color: white;\n    }\n    .particles {\n        margin: 0 0 2em 0;\n        list-style-type: none;\n        padding: 0;\n        width: 15em;\n        float: left;\n    }\n    .particles li {\n        cursor: pointer;\n        position: relative;\n        left: 0;\n        background-color: #EEE;\n        margin: .5em;\n        padding: .3em 0;\n        height: 1.6em;\n        border-radius: 20px 4px 4px 20px;\n    }\n    .particles li.selected:hover {\n        background-color: #BBD8DC !important;\n        color: white;\n    }\n    .particles li:hover {\n        color: #607D8B;\n        background-color: #DDD;\n        left: .1em;\n    }\n    .particles .text {\n        position: relative;\n        top: -3px;\n    }\n    .particles .code {\n        display: inline-block;\n        text-align: center;\n        font-size: small;\n        color: white;\n        width: 1.2em;\n        padding: 0.8em 0.7em 0 0.7em;\n        background-color: #607D8B;\n        line-height: 1em;\n        position: relative;\n        left: -1px;\n        top: -4px;\n        height: 1.8em;\n        margin-right: .8em;\n        border-radius: 20px;\n    }\n    "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
@@ -47,6 +43,25 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                 return Particle;
             }());
             exports_1("Particle", Particle);
+            PARTICLES = [
+                { id: 'u', name: 'up', mass: '2.3MeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+                { id: 'd', name: 'down', mass: '4.8MeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+                { id: 'c', name: 'charm', mass: '1.275GeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+                { id: 's', name: 'strange', mass: '95MeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+                { id: 't', name: 'top', mass: '173.07GeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+                { id: 'b', name: 'bottom', mass: '4.18GeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
+                { id: 'e', name: 'electron', mass: '0.511MeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
+                { id: 'μ', name: 'muon', mass: '105.7MeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
+                { id: 'τ', name: 'tau', mass: '1.777GeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
+                { id: 'νe', name: 'electron neutrino', mass: '<2.2eV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
+                { id: 'νμ', name: 'muon neutrino', mass: '<0.17MeV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
+                { id: 'ντ', name: 'tau neutrino', mass: '<15.5MeV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
+                { id: 'g', name: 'gluon', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Strong'] },
+                { id: 'γ', name: 'photon', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Electromagnetic'] },
+                { id: 'Z', name: 'Z boson', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Weak'] },
+                { id: 'W', name: 'W boson', mass: '0', charge: '0', spin: '±1', type: 'Boson', forces: ['Weak'] },
+                { id: 'H', name: 'Higgs boson', mass: '0', charge: '0', spin: '0', type: 'Boson', forces: [] }
+            ];
         }
     }
 });

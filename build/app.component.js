28affb55447a3c14e3172ba734e7865651f462f9
Gonzalo Trenco <gtrenco@gmail.com>
2017-05-19 11:46:57 +0200

    build update

--- build/app.component.js
+++ build/app.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', './particle-detail/particle-detail.component'], function(exports_1, context_1) {
+System.register(['@angular/core', './particle-detail/particle-detail.component', './particle.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,8 +10,8 @@ System.register(['@angular/core', './particle-detail/particle-detail.component']
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, particle_detail_component_1;
-    var AppComponent, PARTICLES;
+    var core_1, particle_detail_component_1, particle_service_1;
+    var AppComponent;
     return {
         setters:[
             function (core_1_1) {
@@ -19,47 +19,38 @@ System.register(['@angular/core', './particle-detail/particle-detail.component']
             },
             function (particle_detail_component_1_1) {
                 particle_detail_component_1 = particle_detail_component_1_1;
+            },
+            function (particle_service_1_1) {
+                particle_service_1 = particle_service_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
+                function AppComponent(_particleService) {
+                    this._particleService = _particleService;
                     this.titulo = 'Standard model';
-                    this.particles = PARTICLES;
                 }
                 AppComponent.prototype.selectParticle = function (particle) {
                     this.selectedParticle = particle;
+                };
+                AppComponent.prototype.ngOnInit = function () {
+                    var _this = this;
+                    this._particleService
+                        .getParticles()
+                        .then(function (particles) { return _this.particles = particles; });
                 };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
                         templateUrl: 'app/app.component.html',
                         styleUrls: ['app/app.component.css'],
-                        directives: [particle_detail_component_1.ParticleDetailComponent]
+                        directives: [particle_detail_component_1.ParticleDetailComponent],
+                        providers: [particle_service_1.ParticleService]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [particle_service_1.ParticleService])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
-            PARTICLES = [
-                { id: 'u', name: 'up', mass: '2.3MeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
-                { id: 'd', name: 'down', mass: '4.8MeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
-                { id: 'c', name: 'charm', mass: '1.275GeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
-                { id: 's', name: 'strange', mass: '95MeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
-                { id: 't', name: 'top', mass: '173.07GeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
-                { id: 'b', name: 'bottom', mass: '4.18GeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
-                { id: 'e', name: 'electron', mass: '0.511MeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
-                { id: 'μ', name: 'muon', mass: '105.7MeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
-                { id: 'τ', name: 'tau', mass: '1.777GeV/c²', charge: '-1', spin: '1/2', type: 'Lepton', forces: ['Electromagnetic', 'Weak'] },
-                { id: 'νe', name: 'electron neutrino', mass: '<2.2eV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
-                { id: 'νμ', name: 'muon neutrino', mass: '<0.17MeV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
-                { id: 'ντ', name: 'tau neutrino', mass: '<15.5MeV/c²', charge: '0', spin: '1/2', type: 'Lepton', forces: ['Weak'] },
-                { id: 'g', name: 'gluon', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Strong'] },
-                { id: 'γ', name: 'photon', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Electromagnetic'] },
-                { id: 'Z', name: 'Z boson', mass: '0', charge: '0', spin: '1', type: 'Boson', forces: ['Weak'] },
-                { id: 'W', name: 'W boson', mass: '0', charge: '0', spin: '±1', type: 'Boson', forces: ['Weak'] },
-                { id: 'H', name: 'Higgs boson', mass: '0', charge: '0', spin: '0', type: 'Boson', forces: [] }
-            ];
         }
     }
 });
---
+++ build/particle.service.js
@@ -0,0 +1,40 @@
+System.register(['@angular/core', './mock-particles'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, mock_particles_1;
+    var ParticleService;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (mock_particles_1_1) {
+                mock_particles_1 = mock_particles_1_1;
+            }],
+        execute: function() {
+            ParticleService = (function () {
+                function ParticleService() {
+                }
+                ParticleService.prototype.getParticles = function () {
+                    return Promise.resolve(mock_particles_1.PARTICLES);
+                };
+                ParticleService = __decorate([
+                    core_1.Injectable(), 
+                    __metadata('design:paramtypes', [])
+                ], ParticleService);
+                return ParticleService;
+            }());
+            exports_1("ParticleService", ParticleService);
+        }
+    }
+});
+//# sourceMappingURL=particle.service.js.map

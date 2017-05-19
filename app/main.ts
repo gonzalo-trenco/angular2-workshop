ad96b79798ed9a6caefa97efcd28303d52f51385
Gonzalo Trenco <gtrenco@gmail.com>
2017-05-19 13:26:12 +0200

    particle service now uses http request to get particles array

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,11 @@
 import { bootstrap  } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { provide } from '@angular/core';
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
     provide(window, {useValue: window})
 ]);
--- app/particles/particle.service.ts
+++ app/particles/particle.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
-import { PARTICLES } from './mock-particles';
+import { Http } from '@angular/http';
+import { Particle } from './particle';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ParticleService {
-    getParticles() {
-        return Promise.resolve(PARTICLES);
+    private particlesUrl = 'app/mock/particles.json';
+    constructor(private http: Http) { }
+    private handleError(err: any) {
+        console.error('An error occurred:', err);
+        return Promise.reject(err.message || err);
     }
-    getParticle(id) {
+    public getParticles(): Promise<Particle[]> {
+        return this.http.get(this.particlesUrl)
+            .toPromise()
+            .then(response => response.json())
+            .catch(this.handleError);
+    }
+    public getParticle(id) {
         return this.getParticles().then(
             particles => particles.filter(
                 particle => particle.id === id

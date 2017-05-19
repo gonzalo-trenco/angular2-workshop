b6bd463e800bc5d3ff9d9d898414323e2c3df5e9
Gonzalo Trenco <gtrenco@gmail.com>
2017-05-19 11:31:08 +0200

    updated build

--- build/app.component.js
+++ build/app.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/core', './particle-detail/particle-detail.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var AppComponent, Particle, PARTICLES;
+    var core_1, particle_detail_component_1;
+    var AppComponent, PARTICLES;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (particle_detail_component_1_1) {
+                particle_detail_component_1 = particle_detail_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -29,20 +32,15 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1> My app: {{titulo}}</h1>\n    <ul class=\"particles\">\n        <li *ngFor=\"let particle of particles\" \n        (click)=\"selectParticle(particle)\"\n        [class.selected]=\"particle === selectedParticle\">\n            <span class=\"code\">{{particle.id}}</span>\n            {{particle.name}}\n        </li>\n    </ul>\n    <div *ngIf=\"selectedParticle\">\n        <h2>Details for {{selectedParticle.type}} {{selectedParticle.name}}.</h2>\n        <p>Masa: {{selectedParticle.mass}}</p>\n        <p>Carga: {{selectedParticle.charge}}</p>\n        <p>Spin: {{selectedParticle.spin}}</p>\n        <p>Type: {{selectedParticle.type}}</p>\n        <p>Fuerzas: {{selectedParticle.forces.join(', ')}}</p>\n        <div>\n            <label>Ajusta la masa: </label>\n            <input [(ngModel)]=\"selectedParticle.mass\" placeholder=\"masa\">\n        </div>\n    </div>",
-                        styles: ["\n    .selected {\n        background-color: #CFD8DC !important;\n        color: white;\n    }\n    .particles {\n        margin: 0 0 2em 0;\n        list-style-type: none;\n        padding: 0;\n        width: 15em;\n        float: left;\n    }\n    .particles li {\n        cursor: pointer;\n        position: relative;\n        left: 0;\n        background-color: #EEE;\n        margin: .5em;\n        padding: .3em 0;\n        height: 1.6em;\n        border-radius: 20px 4px 4px 20px;\n    }\n    .particles li.selected:hover {\n        background-color: #BBD8DC !important;\n        color: white;\n    }\n    .particles li:hover {\n        color: #607D8B;\n        background-color: #DDD;\n        left: .1em;\n    }\n    .particles .text {\n        position: relative;\n        top: -3px;\n    }\n    .particles .code {\n        display: inline-block;\n        text-align: center;\n        font-size: small;\n        color: white;\n        width: 1.2em;\n        padding: 0.8em 0.7em 0 0.7em;\n        background-color: #607D8B;\n        line-height: 1em;\n        position: relative;\n        left: -1px;\n        top: -4px;\n        height: 1.8em;\n        margin-right: .8em;\n        border-radius: 20px;\n    }\n    "]
+                        templateUrl: 'app/app.component.html',
+                        styleUrls: ['app/app.component.css'],
+                        directives: [particle_detail_component_1.ParticleDetailComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
-            Particle = (function () {
-                function Particle() {
-                }
-                return Particle;
-            }());
-            exports_1("Particle", Particle);
             PARTICLES = [
                 { id: 'u', name: 'up', mass: '2.3MeV/c²', charge: '2/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
                 { id: 'd', name: 'down', mass: '4.8MeV/c²', charge: '1/3', spin: '1/2', type: 'Quark', forces: ['Electromagnetic', 'Strong', 'Weak'] },
---
+++ build/particle.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Particle;
+    return {
+        setters:[],
+        execute: function() {
+            Particle = (function () {
+                function Particle() {
+                }
+                return Particle;
+            }());
+            exports_1("Particle", Particle);
+        }
+    }
+});
+//# sourceMappingURL=particle.js.map

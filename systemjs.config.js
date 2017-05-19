5b16191d3a6c9c904793d18f2d065aef561df90f
Gonzalo Trenco <gtrenco@gmail.com>
2017-05-19 10:36:15 +0200

    init: systemjs now look for build dir

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,7 +5,7 @@
 (function(global) {
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
+    'app':                        'build',
     '@angular':                   'node_modules/@angular',
     'rxjs':                       'node_modules/rxjs'
   };
